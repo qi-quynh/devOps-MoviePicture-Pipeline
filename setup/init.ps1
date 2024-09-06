fb8e5fca9ce9c72e5e9517aa784f1a10505beae5
Quynh <quynhkun27@gmail.com>
2024-09-06 19:58:54 +0700

    fix:update

--- setup/init.ps1
+++ setup/init.ps1
@@ -1,17 +1,14 @@
-#!/bin/bash
-set -e -o pipefail
+# Fetch IAM github-action-user ARN
+Write-Output "Fetching IAM github-action-user ARN"
+$userarn = (aws iam get-user --user-name github-action-user | jq -r .User.Arn)
 
-echo "Fetching IAM github-action-user ARN"
-userarn=arn:aws:iam::310722232770:user/github-action-user
+# Download tool for manipulating aws-auth (Update link if using WSL or different version)
+Write-Output "Downloading tool..."
+Invoke-WebRequest -Uri "https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_windows_amd64.exe" -OutFile "aws-iam-authenticator.exe"
 
-# Download tool for manipulating aws-auth
-echo "Downloading tool..."
-curl -X GET -L https://github.com/kubernetes-sigs/aws-iam-authenticator/releases/download/v0.6.2/aws-iam-authenticator_0.6.2_linux_amd64 -o aws-iam-authenticator
-chmod +x aws-iam-authenticator
+Write-Output "Updating permissions"
+Start-Process -FilePath ".\aws-iam-authenticator.exe" -ArgumentList "add user --userarn=$userarn --username=github-action-role --groups=system:masters --kubeconfig=$HOME/.kube/config --prompt=false" -NoNewWindow -Wait
 
-echo "Updating permissions"
-./aws-iam-authenticator add user --userarn="${userarn}" --username=github-action-role --groups=system:masters --kubeconfig="$HOME"/.kube/config --prompt=false
-
-echo "Cleaning up"
-rm aws-iam-authenticator
-echo "Done!"
+Write-Output "Cleaning up"
+Remove-Item -Path "aws-iam-authenticator.exe"
+Write-Output "Done!"
